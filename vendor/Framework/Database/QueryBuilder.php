a0bd20bd2f91fc216640024a0925563b9b3925c9
mthdht <mthdht@gmail.com>
2018-07-05 11:27:19 +0200

    start query builder class

---
+++ vendor/Framework/Database/QueryBuilder.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author mthdht
+ * @license MIT
+ */
+
+namespace Framework\Database;
+
+
+class QueryBuilder
+{
+
+    private $table;
+    private $fields;
+    private $whereClauses;
+    private $orderBy;
+    private $limit;
+    private $offset;
+
+
+    private function table($table)
+    {
+        $this->table = $table;
+        return $this;
+    }
+
+
+    public static function __callStatic($name, $arguments)
+    {
+
+        $queryBuilder = new QueryBuilder();
+        return call_user_func_array([$queryBuilder, $name], $arguments);
+
+    }
+}
