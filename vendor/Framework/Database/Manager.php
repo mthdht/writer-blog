2cba3e25ed7bb84c65a1bbb2c8bfd469cb5146ae
mthdht <mthdht@gmail.com>
2018-07-06 17:03:07 +0200

    start manager class

---
+++ vendor/Framework/Database/Manager.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author
+ * @license MIT
+ */
+
+namespace Framework\Database;
+
+
+class Manager
+{
+
+    protected $table;
+
+    protected $db
+
+    public function __construct(Database $db)
+    {
+
+    }
+
+    public static function all($column = ['*'])
+    {
+
+    }
+
+    public static function find($id)
+    {
+
+    }
+
+    public static function update($id, $data)
+    {
+
+    }
+
+    public static function delete($id)
+    {
+
+    }
+
+    public static function create($data)
+    {
+
+    }
+
+}
