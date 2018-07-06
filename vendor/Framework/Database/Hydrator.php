88fb5d1fe091093cbea8efdc310310bb96f1b314
mthdht <mthdht@gmail.com>
2018-07-06 16:52:28 +0200

    start hydrator trait

---
+++ vendor/Framework/Database/Hydrator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mthdht
+ * Date: 06/07/18
+ * Time: 16:26
+ */
+
+namespace Framework\Database;
+
+
+trait Hydrator
+{
+
+    public function fill($attributes) {
+
+    }
+}
