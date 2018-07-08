7a69b3ce9b2d431052e2ca1cc1545beca042d4a8
mthdht <mthdht@gmail.com>
2018-07-08 22:15:42 +0200

    made 'all' method and work staticly by facade

--- vendor/Framework/Database/Manager.php
+++ vendor/Framework/Database/Manager.php
@@ -6,22 +6,27 @@
 
 namespace Framework\Database;
 
+use Framework\Database\QueryBuilder;
 
 class Manager
 {
 
     protected $table;
+    protected $builder;
 
-    protected $db
-
-    public function __construct(Database $db)
+    private function __construct()
     {
+        $tableName = explode('\\', get_called_class());
+        $tableName = end ($tableName);
+        $tableName = str_replace('Manager', '', $tableName);
+        $this->table = strtolower($tableName);
+        $this->builder = QueryBuilder::table($this->table);
 
     }
 
-    public static function all($column = ['*'])
+    private function all($column = ['*'])
     {
-
+        return $this->builder->select(implode(', ', $column))->send();
     }
 
     public static function find($id)
@@ -42,6 +47,13 @@ class Manager
     public static function create($data)
     {
 
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        $class = get_called_class();
+        $manager = new $class();
+        return call_user_func_array([$manager, $name], $arguments);
     }
 
 }
