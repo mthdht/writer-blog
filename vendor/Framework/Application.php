a8f755d83f0f7f00b037ee354cd8d5ef1ae61b71
mthdht <mthdht@gmail.com>
2018-07-09 15:13:12 +0200

    remove test from application class

--- vendor/Framework/Application.php
+++ vendor/Framework/Application.php
@@ -9,24 +9,11 @@ namespace Framework;
 use Framework\Database\QueryBuilder;
 use Framework\Routing\Router;
 use Framework\Database\Manager;
-use App\Models\CategoriesManager;
 
 class Application
 {
     public function run()
     {
-        $data2 = CategoriesManager::create([
-            'title' => 'title insert',
-            'content' => 'content insert'
-        ]);
-        var_dump($data2);
-
-        /*Router::get('/', function () {
-            return 'hello';
-        });
-        // lance le router
-        $response = Router::run();
-        echo ($response);*/
 
     }
 }
