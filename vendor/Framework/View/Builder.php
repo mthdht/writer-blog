9bbb46de5c5ca7c8474594eb55ab5e32f3db3348
mthdht <mthdht@gmail.com>
2018-07-10 12:52:38 +0200

    made page builder

---
+++ vendor/Framework/View/Builder.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @author mthdht
+ * @license MIT
+ */
+
+namespace Framework\View;
+
+
+class Builder
+{
+    protected $view;
+    public $matches;
+    public $page;
+
+    public function __construct(View $view)
+    {
+        $this->view = $view;
+    }
+
+
+    public function hasExtend()
+    {
+        $content = file_get_contents($this->view->contentFile);
+        if (preg_match('#@extend\(\'(.+)\'\)#', $content, $matches)) {
+            $this->matches['extend'] = $matches;
+            $this->page = file_get_contents($this->view->basePath . preg_replace('#\.#', '/', $matches[1]) . '.php');
+            return true;
+        }
+        $this->page = file_get_contents($this->view->contentFile);
+        return false;
+
+    }
+
+    public function parseBlock()
+    {
+        $this->page = preg_replace_callback('#@generate\(\'(.+)\'\)(.+)#sU', function ($matches) {
+            preg_match('#@block\(\'' . $matches[1] .'\'\)(.+)@endblock#sU', file_get_contents($this->view->contentFile), $matche);
+            return $matche[1];
+        }, $this->page);
+
+    }
+
+    public function build()
+    {
+        if ($this->hasExtend()) {
+            $this->parseBlock();
+        }
+        $storageFilePath = ROOT . '/storage/' . $this->view->name . '.php';
+        return file_put_contents($storageFilePath, $this->page);
+    }
+
+}
